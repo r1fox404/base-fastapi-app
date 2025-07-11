2677442b523087961cd0310f6bc2cd5440cde6a5
r1fox404 <r.ikt@yandex.ru>
2025-07-11 18:50:35 +0300

    add: Enums for Events, Contracts

--- application/core/enums.py
+++ application/core/enums.py
@@ -1,14 +1,33 @@
 from enum import Enum
 
 
-class UserRole(str, Enum):
+class UserRoleType(str, Enum):
     USER = "USER"
-    WORKER = "WORKER"
     ADMIN = "ADMIN"
+    WORKER = "WORKER"
     MODERATOR = "MODERATOR"
 
 
-class UserStates(str, Enum):
+class UserStateType(str, Enum):
     BLOCKED = "BLOCKED"
     REGISTERED = "REGISTERED"
     ORDER_PROCESSING = "ORDER_PROCESSING"
+
+
+class EventStateType(str, Enum):
+    SOLD_OUT = "SOLD_OUT"
+    COMPLETED = "COMPLETED"
+    REGISTRATION = "REGISTRATION"
+
+
+class ContractStateType(str, Enum):
+    PENDING = "PENDING"
+    ACCEPTED = "ACCEPTED"
+    REJECTED = "REJECTED"
+    INSPECTED = "INSPECTED"
+
+
+class ContractRejectType(str, Enum):
+    USER = "BY_USER"
+    ADMIN = "BY_ADMIN"
+    SYSTEM = "BY_SYSTEM"
